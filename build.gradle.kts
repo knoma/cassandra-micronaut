98274313dd752045dbc697a3640e748f9315dd30
knoma <knoma2010@gmail.com>
2023-06-19 17:56:12 -0400

    bumped up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.micronaut.application") version "3.7.8"
+    id("io.micronaut.application") version "4.0.0-M4"
 }
 
 version = "0.1"
@@ -10,8 +10,10 @@ repositories {
     mavenCentral()
 }
 
+val casandraDriverVersion: String by project
+
+
 dependencies {
-    annotationProcessor("io.micronaut:micronaut-http-validation")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-jackson-databind")
     implementation("io.micronaut.reactor:micronaut-reactor")
@@ -20,15 +22,14 @@ dependencies {
     runtimeOnly("ch.qos.logback:logback-classic")
     compileOnly("org.graalvm.nativeimage:svm")
 
-    implementation("io.micronaut:micronaut-validation")
-
     implementation("io.micronaut.cassandra:micronaut-cassandra")
-    implementation "com.datastax.oss:java-driver-core:$casandraDriverVersion"
-    implementation "com.datastax.oss:java-driver-mapper-processor:$casandraDriverVersion"
-    annotationProcessor "com.datastax.oss:java-driver-mapper-processor:$casandraDriverVersion"
+    implementation("com.datastax.oss:java-driver-core:$casandraDriverVersion")
+    implementation("com.datastax.oss:java-driver-mapper-processor:$casandraDriverVersion")
+    annotationProcessor("com.datastax.oss:java-driver-mapper-processor:$casandraDriverVersion")
 
     // RestAssured dependencies
     testImplementation("io.rest-assured:rest-assured:4.4.0")
+    runtimeOnly("org.yaml:snakeyaml")
 }
 
 
@@ -39,8 +40,7 @@ java {
     sourceCompatibility = JavaVersion.toVersion("17")
     targetCompatibility = JavaVersion.toVersion("17")
 }
-
-graalvmNative.toolchainDetection = false
+graalvmNative.toolchainDetection.set(false)
 micronaut {
     runtime("netty")
     testRuntime("junit5")
